b6057a6228e0e3437fe8d2237789bf7d43ff990a
s-patkar <scpcooldude@gmail.com>
2021-06-24 18:48:49 -0400

    Redone Final Commit (Sources Pages)

--- app.R
+++ app.R
@@ -55,8 +55,8 @@ ui <- navbarPage("Covid TrackR",
                                      helpText(h5("Click to explore dropdown menu.")),
                                      selectInput("var",
                                                  label = "Choose an x-variable to display",
-                                                 choices = c("Population", "Unemployment",
-                                                             "Property_Rights", "Judical_Efficiency_Score",
+                                                 choices = c("Population", "Unemployment_Rate",
+                                                             "Property_Rights_Score", "Judical_Efficiency_Score",
                                                              "Life_Expectancy"),
                                                  selected = "Population"),
                                      selectInput("vary",
@@ -70,22 +70,29 @@ ui <- navbarPage("Covid TrackR",
                                      textOutput("selected_var"),
                                      plotlyOutput("plot2"),
                                      h3("Analysis"),
+                                     br(),
                                      h4("Population vs. Death/Infection Rate:"),
-                                     h5("There seems to be a slight positive correalation between a country's population
+                                     h5("There is a slight positive correalation between a country's population
                                         and it's Covid-19 death toll. This may be because countries with very large
                                         populations tend to be clustered in dense cities where transmission is more likely.
                                         This same correlation does not exist with infection rate."),
+                                     br(),
                                      h4("Unemployment Rate vs. Death/Infection Rate:"),
-                                     h5("There seems to be a strong positive and linear correlation between a country's population
+                                     h5("There is a strong positive and linear correlation between a country's population
                                         and it's Covid-19 death toll. There is no relation between unemployment and infection rates."),
+                                     br(),
                                      h4("Property Rights score vs. Death/Infection Rate:"),
                                      h5("There is a strong positive correlation between a country's property rights score and
                                         infection rate. The opposite is true for death rates."),
+                                     br(),
                                      h4("Judical Efficiency Score vs. Death/Infection Rate:"),
                                      h5("There is no correlation"),
+                                     br(),
                                      h4("Life Expectancy vs. Death/Infection Rate:"),
                                      h5("There is a slight positive correlation between life expectancy and both death and 
-                                        infection rates.")
+                                        infection rates."),
+                                     br(),
+                                     br()
                                      
                               )
                             )
@@ -99,11 +106,13 @@ ui <- navbarPage("Covid TrackR",
                                              h5("Hi I'm Shreeram Patkar, a first-year Medical Science student at the
                                                 University of Western Ontario with an interest in healthcare, computer
                                                 science and robotics."),
+                                             br(),
                                              h3("About the Project"),
                                              h4("Covid-19 TrackR and socio-economic causal analysis"),
                                              h5("This project aims to graphically display the progression of Covid-19
                                                 over the past year by country and to to analyse the impacts of various
                                                 socio-economic metrics and factors on the virus's spread."),
+                                             br(),
                                              h3("Goals"),
                                              h5("- To demonstrate correlations while:"),
                                              h5("- Effectively demonstrating data in an easy-to-read format."),
@@ -117,8 +126,51 @@ ui <- navbarPage("Covid TrackR",
                             
                           )),
                  navbarMenu("Sources",
-                            tabPanel("Source A"),
-                            tabPanel("Source B"))
+                            tabPanel("Source A", 
+                                     fluidPage(
+                                       fluidRow(
+                                         column(4, offset = 3,
+                                                h2("Source A"),
+                                                h4("Source A is data about Covid-19 Daily statistics from the worldometer
+                                                   organization and is retreived from Kaggle.com"),
+                                                br(),
+                                                h5("Scraped Kaggle URL:"),a("https://www.kaggle.com/josephassaker/covid19
+                                                        -global-dataset?select=worldometer_coronavirus_daily_data.csv"),
+                                                h5("Original Source URL:"),a("worldometers.info")
+                                       
+                                     )))),
+                            tabPanel("Source B", 
+                                     fluidPage(
+                                       fluidRow(
+                                         column(4, offset = 3,
+                                                h2("Source B"),
+                                                h4("Source B is data about different metrics in the economic freedom index
+                                                from the world heritage organization and is retreived from Kaggle.com"),
+                                                br(),
+                                                h5("Scraped Kaggle URL:"),a("https://www.kaggle.com/lewisduncan93/
+                                                                            the-economic-freedom-index"),
+                                                h5("Original Source URL:"),a("https://www.heritage.org/index")
+                                                
+                                         )))
+                                     
+                                     ),
+                            tabPanel("Source C", 
+                                     fluidPage(
+                                       fluidRow(
+                                         column(4, offset = 3,
+                                                h2("Source C"),
+                                                h4("Source C is data about life expectancy and various health metrics 
+                                                around the world from the collected by the World Health Organization and
+                                                   retreived from Kaggle.com"),
+                                                br(),
+                                                h5("Scraped Kaggle URL:"),a("https://www.kaggle.com/kumarajarshi/life-expectancy-who"),
+                                                h5("Original Source URL:"),a("https://www.who.int/data/gho/data/indicators/
+                                                                             indicator-details/GHO/life-expectancy-at-birth-(years)")
+                                                
+                                         )))
+                                     
+                                     )
+                            )
 )
 
 
@@ -127,8 +179,8 @@ server <- function(input, output) {
   datax <- reactive({
     switch(input$var,
            Population = short_raw$pop,
-           Unemployment = short_raw$unemp,
-           Property_Rights = short_raw$prop_right, 
+           Unemployment_Rate = short_raw$unemp,
+           Property_Rights_Score = short_raw$prop_right, 
            Judical_Efficiency_Score = short_raw$jud_eff,
            Life_Expectancy = short_raw$life_ex
     )
@@ -153,11 +205,12 @@ server <- function(input, output) {
     econ_plot <- short_raw %>% 
       ggplot(aes(color = country, x= as.numeric(datax()) , y = datay(),
                  text = paste("Country:", country, "\n",
+                              paste(input$var,": "), paste(datax(),ifelse(input$var == "Population", "M", ""),
+                                                           ifelse(input$var == "Unemployment_Rate", "%", "")), "\n",
                               "Death Rate:", paste(format(round(death_rate*100, 2), nsmall = 2), "%"), "\n",
                               "GDPPC:", GDPPC, "\n",
                               "Total Deaths:", cumulative_total_deaths, "\n",
-                              "Total Cases:", cumulative_total_cases, "\n",
-                              "Population:", pop)))+
+                              "Total Cases:", cumulative_total_cases, "\n")))+
       geom_point()+
       geom_smooth()+
       scale_y_continuous(labels = scales::percent_format(accuracy = 1))+
